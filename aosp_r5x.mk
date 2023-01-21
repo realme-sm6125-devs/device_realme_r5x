68c7447584d25c0497b28a9cf269d6bae4191a77
takumi021 <omrameshpatel@gmail.com>
2023-01-21 16:05:27 +0000

    r5x: Init PixelOS

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -9,12 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Awaken stuff
-$(call inherit-product, vendor/awaken/config/common.mk)
-USE_GAPPS := true
-USE_PIXEL_CHARGER := true
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Inherit from r5x device
@@ -27,15 +24,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := awaken_r5x
+PRODUCT_NAME := aosp_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
-
-TARGET_VENDOR_PRODUCT_NAME := r5x
-TARGET_VENDOR_DEVICE_NAME := r5x
-
-# Build Info
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="r5x" \
-    PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
