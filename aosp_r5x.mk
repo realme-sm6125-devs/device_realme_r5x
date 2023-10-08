c166999637fe275af8da5b744451dc84824ae538
takumi021 <omrameshpatel@gmail.com>
2023-10-09 03:31:04 +0530

    r5x: Awaken --> AOSP

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -9,33 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Awaken stuff
-$(call inherit-product, vendor/awaken/config/common.mk)
-USE_GAPPS := true
-USE_PIXEL_CHARGER := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := awaken_r5x
+PRODUCT_NAME := aosp_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 TARGET_VENDOR_PRODUCT_NAME := r5x
 TARGET_VENDOR_DEVICE_NAME := r5x
-
-# Build Info
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="r5x" \
-    PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
